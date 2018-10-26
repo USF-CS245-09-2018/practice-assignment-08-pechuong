d9bf06196a04a285ac56bbbad4a9d2cf722dcbe5
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-10-26 15:11:51 -0700

    Finished get method and correct second add method

--- LinkedList.java
+++ LinkedList.java
@@ -2,8 +2,8 @@ public class LinkedList implements List {
 	
 	public class Node {
 		public Object data;
-		public Object next;
-		public Object prev;
+		public Node next;
+		public Node prev;
 		
 		public Node(Object dataValue) {
 			this.data = dataValue;
@@ -12,7 +12,7 @@ public class LinkedList implements List {
 		}
 	}
 
-	public Object head;
+	public Node head;
 	public int size;
 
 	public LinkedList() {
@@ -24,19 +24,35 @@ public class LinkedList implements List {
 		Node newNode = new Node(obj);
 		newNode.next = this.head;
 		this.head = newNode;	
+		this.size++;
 	}
 
 	public void add(int pos, Object obj) throws Exception { 
-	
+		int listPosition = 0;
+		Node currNode = this.head;
+		while (currNode.data != null && listPosition < pos) {
+			currNode = currNode.next;
+			listPosition++;
+		}
+		currNode.prev.next = new Node(obj);
+		currNode.prev.next.prev = currNode.prev;
+		currNode.prev.next.next = currNode;
+		currNode.prev = currNode.prev.next;
+		this.size++;
 	}
 
 	public Object get(int pos) throws Exception {
-		return null;	
+		int listPosition = 0;
+		Node currNode = this.head;
+		while (listPosition < pos) {
+			currNode = currNode.next;
+			listPosition++;
+		}
+		return currNode;
 	}
 	
 	public Object remove(int pos) throws Exception {
 		return null;	
-	
 	}
 
 	public int size() {
