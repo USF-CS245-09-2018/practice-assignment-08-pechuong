0b5d81b87a432df239cfa7894a8e5fad0c9e5434
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-10-26 14:25:04 -0700

    Added a Node class and then implemented size()

--- LinkedList.java
+++ LinkedList.java
@@ -1,9 +1,23 @@
 public class LinkedList implements List {
 	
-	private int size;
+	public class Node {
+		public Object data;
+		public Object left;
+		public Object right;
+		
+		public Node(Object dataValue) {
+			this.data = dataValue;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+	public Object head;
+	public int size;
 
 	public LinkedList() {
-		
+		this.head = new Node(null);
+		this.size = 0;
 	}
 	
 	public void add(Object obj) throws Exception {
@@ -24,7 +38,7 @@ public class LinkedList implements List {
 	}
 
 	public int size() {
-		return -1;	
+		return this.size;	
 	}
 
 }
