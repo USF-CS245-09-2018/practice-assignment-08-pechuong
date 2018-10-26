8a6246f5de261691557f38ffeaa268972dde9caa
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-10-26 14:11:02 -0700

    Added methods from List.java

---
+++ LinkedList.java
@@ -0,0 +1,30 @@
+public class LinkedList implements List {
+	
+	private int size;
+
+	public LinkedList() {
+		
+	}
+	
+	public void add(Object obj) throws Exception {
+	
+	}
+
+	public void add(int pos, Object obj) throws Exception { 
+	
+	}
+
+	public Object get(int pos) throws Exception {
+		return null;	
+	}
+	
+	public Object remove(int pos) throws Exception {
+		return null;	
+	
+	}
+
+	public int size() {
+		return -1;	
+	}
+
+}
